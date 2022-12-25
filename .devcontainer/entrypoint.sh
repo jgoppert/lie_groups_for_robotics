5b2e16445f2741c1818c5034443eb5311f932aa0
James Goppert <james.goppert@gmail.com>
2022-12-25 18:49:28 -0500

    Initial commit.

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "hello lgr"
+. ~/.venv-lgfr/bin/activate
+/bin/sh -c "bash"
---
+++ .devcontainer/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+	bash-completion \
+	build-essential \
+	curl \
+	git \
+	python3-pip \
+	python3-setuptools \
+	python3-venv \
+	unzip \
+	wget \
+	vim
+
+python3 -m venv --prompt lgfr ~/.venv-lgfr
+source ~/.venv-lgfr/bin/activate
+pip3 install --no-cache-dir -r /tmp/requirements.txt
+
+
